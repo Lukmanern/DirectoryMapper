50fed6f7e2728791b830b6b65f9d13857b5773c1
LukmanE22 <unsurlukman@gmail.com>
2023-01-09 21:34:57 +0700

    make more effective in main-conditionals

--- main.go
+++ main.go
@@ -1,43 +1,50 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"path/filepath"
 )
 
-var baseDir string = "C:/xampp/htdocs/mengundang/app"
-
 func main() {
-	baseDir = "C:/Users/Lenovo/OneDrive/Documents/Dev Go Directory Mapper"
-	root := baseDir
-	printDirectoryMap(1, root, "   ", ".git")
+	var baseDir string = "C:/xampp/htdocs/mengundang/app"
+	// baseDir = "C:/Users/Lenovo/OneDrive/Documents/Dev Go Directory Mapper"
+	printDirectoryMap(1, baseDir, "   ", ".git")
 }
 
-func printDirectoryMap(counter int, path, prefix, exclude string) error {
+func ErrorHandler(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func printDirectoryMap(counter int, path, prefix, exclude string) {
 	if counter >= 8 {
-		return nil
+		fmt.Println("::::::::::MAX PRINT::::::::::")
+		return
 	}
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		return err
+		err = errors.New(fmt.Sprint(err.Error(), "at counter:", counter))
+		ErrorHandler(err)
 	}
 	for i, file := range files {
 		if exclude == file.Name() {
 			continue
 		}
+		// 3 white space
+		space := "   "
 		if i == len(files)-1 {
 			fmt.Println(prefix + "└── " + file.Name())
-			if file.IsDir() {
-				printDirectoryMap(counter+1, filepath.Join(path, file.Name()), prefix+"    "+"", exclude)
-			}
+			space = " " + space
 		} else {
 			fmt.Println(prefix + "├── " + file.Name())
-			if file.IsDir() {
-				printDirectoryMap(counter+1, filepath.Join(path, file.Name()), prefix+"│   "+"", exclude)
-			}
+			space = "│" + space
+		}
+		if file.IsDir() {
+			printDirectoryMap(counter+1, filepath.Join(path, file.Name()), prefix+space, exclude)
 		}
 	}
-
-	return nil
 }
